f171a010d6c7142342091f4b38acb7226e61d7db
NihadHasan24 <nihadhasanniloy24241124@gmail.com>
2025-07-05 16:57:15 +0600

    Time: 3 ms (34.37%), Space: 17.8 MB (50.63%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.py
@@ -1,13 +1,22 @@
 class Solution:
     def findLucky(self, arr: List[int]) -> int:
-        res = {}
-        for i in range(len(arr)):
-            if arr[i] not in res:
-                res[arr[i]] = 1
-            else:
-                res[arr[i]] += 1
-        maxi = -1
-        for num,freq in res.items():
-            if num == freq:
-                maxi = max(maxi,num)
-        return maxi    
+        freq = [0] * 501
+        for i in arr:
+            freq[i] += 1
+        for i in range(500,0,-1):
+            if freq[i] == i:
+                return i
+        return -1
+
+        # HASHMAP            
+        # res = {}
+        # for i in range(len(arr)):
+        #     if arr[i] not in res:
+        #         res[arr[i]] = 1
+        #     else:
+        #         res[arr[i]] += 1
+        # maxi = -1
+        # for num,freq in res.items():
+        #     if num == freq:
+        #         maxi = max(maxi,num)
+        # return maxi    
